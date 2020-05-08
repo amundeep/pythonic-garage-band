f628f0a5c4515ae64d465fd12ca32d842d8e1ea3
Amundeep Singh <amundeep@amazon.com>
2020-05-07 21:07:23 -0700

    Lab 04 submission

---
+++ pythonic_garage_band/garage_band.py
@@ -0,0 +1,78 @@
+from abc import ABC, abstractmethod
+
+class Band:
+    """ Band Class """
+    instances = []
+
+    def __init__(self, name, members):
+        self.name = name
+        self.members = members
+        Band.instances.append(self)
+
+    def __repr__(self):
+        """ Object Representation """
+        return "{'name': " + self.name + ", 'members': [" + str(self.members) + "]}"
+
+    def __str__(self):
+        """ String representation """
+        return "Band(" + self.name + ", " + str(self.members) + ")"
+
+    # play solos in the order they were added
+    def play_solos(self):
+        solos = ""
+        for m in self.members:
+            solos = solos + m.play_solo() + " "
+        return solos.strip()
+
+    @staticmethod
+    def to_list():
+        return Band.instances
+
+    @classmethod
+    def create_from_data(cls, data):
+        # create from some data source
+        pass
+
+class Musician(ABC):
+    """ Musician Abstract Class """
+    def __init__(self, name, instrument):
+        self.name = name
+        self.instrument = instrument
+
+    def __repr__(self):
+        return "{'name': " + self.name + ", 'instrument': " + self.instrument + "}"
+
+    def __str__(self):
+        return "Musician(" + self.name + ", " + self.instrument + ")"
+
+    def get_instrument(self):
+        return self.instrument
+
+    @abstractmethod
+    def play_solo(self):
+        pass
+
+""" Below are subclasses of Musician """
+class Drummer(Musician):
+    def __init__(self, name, solo="BADDUM TSS"):
+        super().__init__(name, "Drumset")
+        self.solo = solo
+
+    def play_solo(self):
+        return self.solo
+
+class Guitarist(Musician):
+    def __init__(self, name, solo="DI DI DOO DA"):
+        super().__init__(name, "Guitar")
+        self.solo = solo
+
+    def play_solo(self):
+        return self.solo
+
+class Singer(Musician):
+    def __init__(self, name, solo="LET IT GO"):
+        super().__init__(name, "Voice")
+        self.solo = solo
+
+    def play_solo(self):
+        return self.solo
--- tests/test_pythonic_garage_band.py
+++ tests/test_pythonic_garage_band.py
@@ -1,5 +1,51 @@
 from pythonic_garage_band import __version__
-
+from abc import ABC, abstractmethod
+from pythonic_garage_band.garage_band import Band, Guitarist, Singer, Drummer
+import pytest
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_guitarist():
+    john = Guitarist("John Mayer")
+    assert john.name == "John Mayer"
+    assert john.instrument == "Guitar"
+    assert john.play_solo() == "DI DI DOO DA"
+
+def test_get_instrument():
+    travis = Drummer("Travis Barker")
+    assert travis.get_instrument() == "Drumset"
+
+def test_musician_repr():
+    slash = Guitarist("Slash")
+    assert slash.__repr__() == "{'name': Slash, 'instrument': Guitar}"
+
+def test_musician_str():
+    slash = Guitarist("Slash")
+    assert slash.__str__() == "Musician(Slash, Guitar)" 
+
+def test_band_to_list():
+    band1 = Band("One Man Yes Band", [Guitarist("Jim Carey")])
+    band2 = Band("Obi-Wan Kenoband", [Drummer("Anakin")])
+    assert len(Band.to_list()) == 2
+
+def test_band_name(coldplay):
+    assert coldplay.name == "Coldplay"
+
+def test_play_solos(coldplay):
+    assert coldplay.play_solos() == "DI DI DOO DA LET IT GO BADDUM TSS"
+
+# def test_band_repr(coldplay):
+#     assert coldplay.members == ""
+
+@pytest.fixture
+# set up fixed object in pytest
+def coldplay():
+    coldplay = Band("Coldplay", [
+        Guitarist("Guy Berryman"),
+        Singer("Chris Martin"),
+        Drummer("Will Champion"),
+    ])
+
+    return coldplay
+
